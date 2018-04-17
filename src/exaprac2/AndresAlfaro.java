d43ff6d292684a72123c61bc2617271d204f8405
PracticaEv2 <PracticaEv2@172.16.15.137>
2018-04-17 13:09:28 -0600

    Examen Pràctico 2 AMAYTS

---
+++ src/exaprac2/AndresAlfaro.java
@@ -0,0 +1,20 @@
+
+package exaprac2;
+
+/**
+ * Nombe de la clase: AndresAlfaro 
+ * Fecha: 17-04-2018
+ * Version: 1.0
+ * CopyRight: ITCA-FEPADE
+ * @author Andrès Alfaro
+ */
+public class AndresAlfaro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
